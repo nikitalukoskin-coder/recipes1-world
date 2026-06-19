331b4e84998eab44908b2918d97831ae54e9b1c5
nikitalukoskin-coder <nikitalukoskin@gmail.com>
2026-06-19 12:42:30 +0600

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Recipes World",
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         <main className="max-w-7xl mx-auto px-4 py-8">{children}</main>
+        <Footer />
       </body>
     </html>
   );
